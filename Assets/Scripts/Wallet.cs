a4f9e34a730a8bb589ad74bc06389e99eb0731a5
Dmitry <Dimidril@users.noreply.github.com>
2020-12-19 18:05:59 +0300

    Подправление
    Поправлено форматирование
    Убрано лишнее событие

--- Assets/Scripts/Wallet.cs
+++ Assets/Scripts/Wallet.cs
@@ -3,13 +3,10 @@ using UnityEngine.Events;
 
 public class Wallet : MonoBehaviour
 {
-    [SerializeField] private UnityEvent _coinAdded;
-
     private int _amount = 0;
 
     public void AddCoin()
     {
         _amount++;
-        _coinAdded?.Invoke();
     }
 }
